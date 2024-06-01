5a6e5569b6c5ab11e5847df0dae412c8559c4845
PDeimel <philipp.deimel@fh-bielefeld.de>
2024-06-01 14:04:00 +0200

    CSS angepasst

---
+++ Frontend/strade/src/Trading.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './LoggedIn.css';
+import './Trading.css';
+
+export default function Trading() {
+
+
+    return(
+        <h2>HOLY MOLY</h2>
+    );
+}
