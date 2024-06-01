436f4921729b63831a3cb10def8e2c7aec52c495
PDeimel <philipp.deimel@fh-bielefeld.de>
2024-06-01 13:39:56 +0200

    TODO message added

--- Frontend/strade/src/Subscription.js
+++ Frontend/strade/src/Subscription.js
@@ -6,9 +6,9 @@ const Subscription = () => {
   const [activeTab, setActiveTab] = useState('Yearly');
   const [buttonText, setButtonText] = useState({
     Yearly: {
-      basic: '100$ / Year',
-      silver: '200$ / Year',
-      gold: '300$ / Year'
+      basic: '99$ / Year',
+      silver: '199$ / Year',
+      gold: '299$ / Year'
     },
     Monthly: {
       basic: '8$ / Month',
