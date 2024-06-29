1160637163dbb07c69ea5192ce83a1d3c502c67d
simon <simon.fedrau@gmx.de>
2024-06-29 14:53:29 +0200

    Hompage.css

--- deployToAWS/deployToAws.sh
+++ deployToAWS/deployToAws.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ssh -i /home/simon/Schreibtisch/Server/Produkt.pem ubuntu@ec2-51-20-249-18.eu-north-1.compute.amazonaws.com 'docker stop $(docker ps -a -q)'
+ssh -i /home/simon/Schreibtisch/Server/Produkt.pem ubuntu@ec2-51-20-249-18.eu-north-1.compute.amazonaws.com 'docker-compose pull'
 ssh -i /home/simon/Schreibtisch/Server/Produkt.pem ubuntu@ec2-51-20-249-18.eu-north-1.compute.amazonaws.com 'docker-compose down'
 ssh -i /home/simon/Schreibtisch/Server/Produkt.pem ubuntu@ec2-51-20-249-18.eu-north-1.compute.amazonaws.com 'docker-compose stop'
 ssh -i /home/simon/Schreibtisch/Server/Produkt.pem ubuntu@ec2-51-20-249-18.eu-north-1.compute.amazonaws.com 'docker-compose up --build -d'
